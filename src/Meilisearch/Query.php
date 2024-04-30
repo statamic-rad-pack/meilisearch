6acf77f52216fc01e285c7c7f7fbd98872f0628f
helloiamlukas <helloiamlukas@fastmail.com>
2024-04-30 16:12:47 +0200

    Implement `orderBy`

--- src/Meilisearch/Query.php
+++ src/Meilisearch/Query.php
@@ -6,14 +6,27 @@ use Statamic\Search\QueryBuilder;
 
 class Query extends QueryBuilder
 {
+    /**
+     * The options to be sent with the search request.
+     * See https://www.meilisearch.com/docs/reference/api/search#body for available options.
+     */
+    public array $options = ['hitsPerPage' => 1000000, 'showRankingScore' => true];
+
     public function getSearchResults($query)
     {
-        $results = $this->index->searchUsingApi($query);
+        $results = $this->index->searchUsingApi($query, $this->options);
 
         return $results->map(function ($result, $i) {
             $result['search_score'] = (int) ceil($result['_rankingScore'] * 1000);
 
             return $result;
         });
+    }
+
+    public function orderBy($column, $direction = 'asc')
+    {
+        $this->options['sort'][] = "{$column}:{$direction}";
+
+        return parent::orderBy($column, $direction);
     }
 }
