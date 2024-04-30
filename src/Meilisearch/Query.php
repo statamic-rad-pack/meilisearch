ab53cc9d23a8a8bc3daab6c0656de9090c258a98
helloiamlukas <helloiamlukas@fastmail.com>
2024-04-30 18:55:09 +0200

    Implement `where` functions

--- src/Meilisearch/Query.php
+++ src/Meilisearch/Query.php
@@ -2,6 +2,7 @@
 
 namespace StatamicRadPack\Meilisearch\Meilisearch;
 
+use Illuminate\Support\Collection;
 use Statamic\Search\QueryBuilder;
 
 class Query extends QueryBuilder
@@ -28,5 +29,107 @@ class Query extends QueryBuilder
         $this->options['sort'][] = "{$column}:{$direction}";
 
         return parent::orderBy($column, $direction);
+    }
+
+    public function where($column, $operator = null, $value = null, $boolean = 'and')
+    {
+        if (! in_array($operator, ['=', '!=', '>', '>=', '<', '<='])) {
+            $value = $operator;
+            $operator = '=';
+        }
+
+        $indexOfLastWhere = array_key_last($this->options['filter'] ?? []);
+
+        if ($boolean === 'and' or ($boolean === 'or' and $indexOfLastWhere === null)) {
+            $this->options['filter'][] = "{$column} {$operator} {$this->escapeValue($value)}";
+        } else {
+            $this->options['filter'][$indexOfLastWhere] = "{$this->options['filter'][$indexOfLastWhere]} OR {$column} {$operator} {$this->escapeValue($value)}";
+        }
+
+        return $this;
+    }
+
+    public function whereIn($column, $values, $boolean = 'and')
+    {
+        $values = $this->prepareValues($values);
+
+        $indexOfLastWhere = array_key_last($this->options['filter'] ?? []);
+
+        if ($boolean === 'and' or ($boolean === 'or' and $indexOfLastWhere === null)) {
+            $this->options['filter'][] = "{$column} IN [{$values}]";
+        } else {
+            $this->options['filter'][$indexOfLastWhere] = "{$this->options['filter'][$indexOfLastWhere]} OR {$column} IN [{$values}]";
+        }
+
+        return $this;
+    }
+
+    public function whereNotIn($column, $values, $boolean = 'and')
+    {
+        $values = $this->prepareValues($values);
+
+        $indexOfLastWhere = array_key_last($this->options['filter'] ?? []);
+
+        if ($boolean === 'and' or ($boolean === 'or' and $indexOfLastWhere === null)) {
+            $this->options['filter'][] = "{$column} NOT IN [{$values}]";
+        } else {
+            $this->options['filter'][$indexOfLastWhere] = "{$this->options['filter'][$indexOfLastWhere]} OR {$column} NOT IN [{$values}]";
+        }
+
+        return $this;
+    }
+
+    public function whereNull($column, $boolean = 'and', $not = false)
+    {
+        $indexOfLastWhere = array_key_last($this->options['filter'] ?? []);
+
+        $operator = $not ? 'IS NOT NULL' : 'IS NULL';
+
+        if ($boolean === 'and' or ($boolean === 'or' and $indexOfLastWhere === null)) {
+            $this->options['filter'][] = "{$column} {$operator}";
+        } else {
+            $this->options['filter'][$indexOfLastWhere] = "{$this->options['filter'][$indexOfLastWhere]} OR {$column} {$operator}";
+        }
+
+        return $this;
+    }
+
+    public function whereBetween($column, $values, $boolean = 'and', $not = false)
+    {
+        $values = $this->prepareValues($values);
+
+        $indexOfLastWhere = array_key_last($this->options['filter'] ?? []);
+
+        $not = $not ? 'NOT ' : '';
+
+        if ($boolean === 'and' or ($boolean === 'or' and $indexOfLastWhere === null)) {
+            $this->options['filter'][] = "{$not}{$column} TO {$values}";
+        } else {
+            $this->options['filter'][$indexOfLastWhere] = "{$this->options['filter'][$indexOfLastWhere]} OR {$not}{$column} TO {$values}";
+        }
+
+        return $this;
+    }
+
+    protected function escapeValue($value)
+    {
+        if (is_string($value)) {
+            $escapedValue = addslashes($value);
+
+            return "'{$escapedValue}'";
+        }
+
+        return $value;
+    }
+
+    protected function prepareValues($values)
+    {
+        if ($values instanceof Collection) {
+            $values = $values->map(fn ($value) => $this->escapeValue($value))->implode(', ');
+        } else {
+            $values = collect($values)->map(fn ($value) => $this->escapeValue($value))->implode(', ');
+        }
+
+        return $values;
     }
 }
